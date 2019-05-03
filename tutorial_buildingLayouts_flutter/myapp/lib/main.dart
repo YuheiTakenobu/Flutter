4e035784cfb815b7b7e158318d74b7a3b6c81d23
YuheiTakenobu <b1016127@fun.ac.jp>
2019-05-04 08:56:39 +0900

    step2 Done

---
+++ tutorial_buildingLayouts_flutter/myapp/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,//謎
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+
+            ),
+
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text('41')
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
